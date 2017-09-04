c688c688c01d323a0279db87f6093b8838ee3267
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-09-04 13:18:50 +0300

    I added scripts and ran them through the autoprefair

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".search-form");
+link.addEventListener('click', function() {
+          popup.classList.toggle("visually-hidden");
+});
