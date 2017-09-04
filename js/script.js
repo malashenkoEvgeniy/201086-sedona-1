270f4e79688714d0f8246545d984ab5ef3f9f3fc
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-09-04 23:03:44 +0300

    first test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var link = document.querySelector(".search-btn");
 var popup = document.querySelector(".search-form");
-link.addEventListener('click', function() {
+link.addEventListener('click', function(evt) {
+          evt.preventDefault();
           popup.classList.toggle("visually-hidden");
 });
