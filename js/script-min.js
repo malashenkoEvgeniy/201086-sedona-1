a0624879ffac91c14efe82d44faf91edd9f0ff88
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-09-07 01:58:45 +0300

    put a background on the map, corrected interactive elements and minimized files

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+<script>var link=document.querySelector(".search-btn"); var popup=document.querySelector(".search-form"); link.addEventListener('click',function(evt){ evt.preventDefault(); popup.classList.toggle("visually-hidden"); }); </script>
--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var link = document.querySelector(".search-btn");
 var popup = document.querySelector(".search-form");
 
 link.addEventListener('click', function(evt) {
-          evt.preventDefault();
-          popup.classList.toggle("visually-hidden");
+			evt.preventDefault();
+			popup.classList.toggle("visually-hidden");
 });
